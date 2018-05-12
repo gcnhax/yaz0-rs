dd0c4bb64104ee7cf34918bfeec5f52bf3f9e819
Erin <sylphofelectricity@gmail.com>
2018-05-12 11:59:26 -0500

    improve error types, move them to their own module

---
+++ src/error.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::fmt;
+use std::error::Error as StdError;
+use std::borrow::Cow;
+use std::convert::From;
+
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    InvalidMagic,
+}
+
+impl Error {
+    fn detail(&self) -> Cow<str> {
+        match *self {
+            Error::Io(ref io_err) => format!("IO error: {}", io_err.description()).into(),
+            _ => self.description().into(),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        fmt.write_str(&self.detail())
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Io(ref io_err) => io_err.description(),
+            Error::InvalidMagic => "Invalid magic in file header",
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,7 @@ extern crate byteorder;
 
 mod inflate;
 mod deflate;
+mod error;
 
 pub use inflate::Yaz0;
-
-use std::convert::From;
-
-#[derive(Debug)]
-pub enum Error {
-    Io(std::io::Error),
-    InvalidMagic,
-}
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Error {
-        Error::Io(err)
-    }
-}
+pub use error::Error;
