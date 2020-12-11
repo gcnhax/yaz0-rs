a9bc7b8ce32e73aff770c169b80e6a9d58a28543
Erin Moon <erin@hecke.rs>
2020-12-11 05:06:30 -0600

    switch Error enum to thiserr

--- src/error.rs
+++ src/error.rs
@@ -1,43 +1,11 @@
-use std::io;
-use std::fmt;
-use std::error::Error as StdError;
-use std::convert::From;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
     /// An error was encountered performing IO operations.
-    Io(io::Error),
+    #[error("backing i/o error")]
+    Io(#[from] std::io::Error),
     /// The Yaz0 file header's magic was invalid.
+    #[error("yaz0 header magic invalid")]
     InvalidMagic,
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-        Error::Io(err)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        match self {
-            Error::Io(io_err) => write!(f, "IO error: {}", io_err),
-            _ => f.write_str(self.description()),
-        }
-    }
-}
-
-impl StdError for Error {
-    fn description(&self) -> &str {
-        match self {
-            Error::Io(io_err) => io_err.description(),
-            Error::InvalidMagic => "Invalid magic in file header",
-        }
-    }
-
-    fn cause(&self) -> Option<&dyn StdError> {
-        match self {
-            Error::Io(io_err) => Some(io_err),
-            _ => None,
-        }
-    }
 }
